a97243ec09549bfad6767f43b83ef03767081c60
Dharmesh patel <93909226+Dharmeshp1582@users.noreply.github.com>
2024-06-28 20:45:07 +0530

    Add files via upload

---
+++ 11.classes and objects/app.js
@@ -0,0 +1,45 @@
+// const student = {
+//     fullName: "Dharmesh Patel",
+//     marks: 90.4,
+//     printMarks : function(){
+//         console.log("marks = ",this.marks);
+//     } ,
+// };
+
+const Employee = {
+    calcTax1(){
+        console.log("tax rate is 10%");
+    },
+
+};
+
+const karanArjun = {
+    salary: 50000,
+};
+
+
+const karanArjun2 = {
+    salary: 50000,
+};
+
+
+const karanArjun3 = {
+    salary: 50000,
+};
+
+
+const karanArjun4 = {
+    salary: 50000,
+};
+
+
+const karanArjun5 = {
+    salary: 50000,
+};
+
+//prototypes for karanArjun
+karanArjun.__proto___ = Employee;
+karanArjun.__proto___ = Employee;
+karanArjun.__proto___ = Employee;
+karanArjun.__proto___ = Employee;
+karanArjun.__proto___ = Employee;
